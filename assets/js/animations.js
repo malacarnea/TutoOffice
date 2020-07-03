58f00bed78ec98d4b782e4e9e3432396e42ab363
Alicia <alicia@localhost.localdomain>
2020-07-03 15:12:11 +0200

    add js animations

---
+++ assets/js/animations.js
@@ -0,0 +1,20 @@
+const ratio = .1
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: .5
+}
+
+const handleIntersect = function (entries, observer) {
+    entries.forEach(function (entry) {
+		if(entry.intersectionRatio>ratio){
+//        if (entry.isIntersecting) {
+            entry.target.classList.add('reveal-visible')
+            observer.unobserve(entry.target)     
+        }
+    })
+}
+const observer = new IntersectionObserver(handleIntersect, options)
+document.querySelectorAll('[class*="reveal-"]').forEach(function(r){
+    observer.observe(r)
+})
